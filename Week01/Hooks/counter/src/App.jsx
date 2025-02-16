a43d18c0cae580aa34a3de270b4fae5488fc0371
Ashirwad Baniyal <ashirwadbaniyal789@gmail.com>
2025-02-16 12:00:06 +0530

    Counter

---
+++ Week01/Hooks/counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react' //Hooks comes from this
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  let [counter, setCounter] = useState(15)
+/*   In React, state variables are used to manage dynamic data that should cause a re-render when changed.
+     whenever we use useState (it can take any data type and function(not good to use)) and return an array first one is the value that usestate contain second one is a function that will be applied on it.
+*/
+  const addValue=()=>{
+    // counter++;
+    setCounter(++counter);
+     console.log(counter);
+  }
+  const removeValue=()=>{
+    setCounter(--counter);
+     console.log(counter);
+  }
+
+  return (
+    <>
+     <h1>Chai aur react</h1>
+     <h2>Counter Value:{counter}</h2>
+     <button 
+     onClick={addValue}
+     >Add Value</button>
+     <button
+     onClick={removeValue}
+     >Remove Value</button>
+    </>
+  )
+}
+
+export default App
